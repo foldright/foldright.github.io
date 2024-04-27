7577ccf399f1ffb3379b67f6439a52b635121a6b
zavakid <zava.kid@gmail.com>
2024-04-28 00:03:28 +0800

    chore: fix error link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/foldright',
             },
           ],
         },
